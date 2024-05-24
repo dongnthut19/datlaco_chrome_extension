d731ad5e085e47e4279d2a55a0a570b232d0fdf5
dongnguyentat <dong.nguyen@mycleo.net>
2024-05-24 22:52:15 +0700

    init project

---
+++ src/app/assets/js/const.js
@@ -0,0 +1,6 @@
+﻿var cartURL = "http://www.datlaco.vn/gio-hang";
+var apiURL= "http://admin.datlaco.vn/api/";
+//var apiURL= "http://localhost:49271/api/";
+var webUrlLogin = "http://www.datlaco.vn/dang-nhap";
+var ndt_vnd = 3700;
+var serviceFee = 10000;
